ae06d8e65b0537abb15bd1ed006b89e3ed011ae6
Akilesh Bapu <akilesh@berkeley.edu>
2017-04-16 16:11:29 -0700

    Finished Storyboard

---
+++ Lab7/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Lab7
+//
+//  Created by Akilesh Bapu on 4/16/17.
+//  Copyright © 2017 org.iosdecal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
