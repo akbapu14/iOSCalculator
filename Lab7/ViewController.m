4f04ea31366f090b2e05cc6fd9f480ccf2378c52
Akilesh Bapu <akilesh@berkeley.edu>
2017-04-16 23:56:28 -0700

    Code Beautifully Abstracted Away

--- Lab7/ViewController.m
+++ Lab7/ViewController.m
@@ -7,18 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import "CalculatorBrain.h"
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet UILabel *calculationView;
-@property NSString *calculationString;
-@property NSString *savedOperand;
-typedef enum actionType : long {
-    multiply = 0,
-    subtract,
-    add,
-    equal
-} actionType;
-@property actionType currentAction;
+@property CalculatorBrain *Cbrain;
 
 @end
 
@@ -27,9 +19,7 @@ typedef enum actionType : long {
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    _calculationString = @"0.0";
-    _savedOperand = @"0.0";
-    _currentAction = multiply;
+    _Cbrain = [[CalculatorBrain alloc] init];
 }
 
 
@@ -39,99 +29,32 @@ typedef enum actionType : long {
 }
 
 - (void)updatecalculationView {
-    _calculationView.text = _calculationString;
-}
-- (void)saveOperand {
-    _savedOperand = _calculationString;
-    _calculationString = @"0.0";
+    [_calculationView setText:[_Cbrain calculationString]];
 }
 
 - (IBAction)numberPressed:(id)sender {
     UIButton *pressedButton = (UIButton *)sender;
-    if (pressedButton.tag == 11) {
-        if ([_calculationString containsString:@"."] && !([_calculationString doubleValue] == 0)) {
-            return;
-        }
-        if ([_calculationString intValue] == 0) {
-            _calculationString = @"0.";
-        } else {
-            _calculationString = [_calculationString stringByAppendingString:@"."];
-        }
-    } else {
-        if ([_calculationString doubleValue] == 0 && ![_calculationString isEqualToString:@"0."]) {
-            if (pressedButton.tag == 0) {
-                
-            } else {
-            _calculationString = [NSString stringWithFormat:@"%ld",(long)pressedButton.tag];
-            }
-
-        } else {
-            _calculationString = [_calculationString stringByAppendingString:[NSString stringWithFormat:@"%ld",(long)pressedButton.tag]];
-        }
-    }
+    [_Cbrain updateCalculationStringFromButtonTag:(int)pressedButton.tag];
     [self updatecalculationView];
 }
 - (IBAction)clearPressed:(id)sender {
-    _calculationString = @"0.0";
+    [_Cbrain clearCalculationString];
     [self updatecalculationView];
 }
 - (IBAction)plusMinusPressed:(id)sender {
-    if([_calculationString hasPrefix:@"-"]) {
-        _calculationString = [_calculationString substringFromIndex:1];
-    } else {
-        _calculationString = [@"-" stringByAppendingString:_calculationString];
-    }
+    [_Cbrain negateCalculationString];
     [self updatecalculationView];
 }
-- (BOOL)hasDecimalPlaces: (double)operand1 _:(double)operand2 {
-    if ((int)operand1 != operand1) {
-        return true;
-    }
-    if ((int)operand2 != operand2) {
-        return true;
-    }
-    return false;
-}
+
 - (IBAction)tangent:(id)sender {
-    double input = [_calculationString doubleValue];
-    _calculationString = [NSString stringWithFormat:@"%.02f",tan(input) ];
-    [self updatecalculationView];
-}
-- (void)commitAction {
-    double operand1 = [_savedOperand doubleValue];
-    double operand2 = [_calculationString doubleValue];
-    double total = 0;
-    switch (_currentAction) {
-        case multiply: {
-            total = operand1 * operand2;
-            break;
-        }
-        case subtract:
-            total = operand1 - operand2;
-            break;
-        case add:
-            total = operand1 + operand2;
-            break;
-        default:
-            break;
-    }
-    if ([self hasDecimalPlaces:operand1 _:operand2]) {
-        _calculationString = [NSString stringWithFormat:@"%.02f", total];
-    } else {
-        _calculationString = [NSString stringWithFormat:@"%d", (int)total];
-    }
+    [_Cbrain tangentCalculationString];
     [self updatecalculationView];
 }
 - (IBAction)actionPressed:(id)sender {
-    switch (((UIButton *)sender).tag) {
-        case equal:
-            [self commitAction];
-            break;
-        default:
-            [self saveOperand];
-            _currentAction = ((UIButton *)sender).tag;
-            break;
-    }
+    UIButton *pressedButton = (UIButton *)sender;
+    if ([_Cbrain recognizeAction:(int)pressedButton.tag]) {
+        [self updatecalculationView];
+    };
 }
 
 @end
